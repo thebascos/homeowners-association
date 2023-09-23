4d9115a990340d9c053f3d3e473ed14d93a3f66e
Princh Canal <princh.canal@craftworks.at>
2023-09-23 19:28:59 +0800

    chore(deployment): adds env vars

--- hoa-backend/src/main.ts
+++ hoa-backend/src/main.ts
@@ -4,8 +4,10 @@ import { CustomExceptionFilter } from './auth/dto/custom-exception.filter';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  app.enableCors({ origin: 'http://localhost:4200' });
-  await app.listen(3000);
+  app.enableCors({
+    origin: process.env.FRONTEND_URL ?? 'http://localhost:4200',
+  });
+  await app.listen(process.env.BASE_URL ?? 3000);
 
   app.useGlobalFilters(new CustomExceptionFilter());
 }
